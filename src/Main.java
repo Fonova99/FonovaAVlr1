c08e15ef6038908fa1b026704ea85671001ce280
Arina Fonova <fonova99@list.ru>
2023-09-27 16:42:28 +0300

    запуск методов двусвязного списка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyLinkedList linkedList1 = new MyLinkedList();
+        MyLinkedList linkedList2 = new MyLinkedList();
+        System.out.println("ДВУСВЯЗНЫЙ СПИСОК");
+        linkedList1.add(1);
+        linkedList1.add(2);
+        linkedList1.add(3);
+        linkedList1.add(4);
+        linkedList1.add(5);
+        linkedList1.print();
+        System.out.println("__________________");
+        linkedList2.add(6);
+        linkedList2.add(7);
+        linkedList2.add(8);
+        linkedList2.add(9);
+        linkedList2.print();
+        System.out.println("__________________");
+
+
+        linkedList1.addAllLast(linkedList2);
+        linkedList1.print();
+        System.out.println("__________________");
+
+
+//        linkedList.addFirst(7);
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        linkedList.addLast(7);
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        linkedList.removeLast();
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        linkedList.addForIndex(2, 5);
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        System.out.println(linkedList.get(2));
+//        System.out.println("__________________");
+//
+//        linkedList.removeAt(2);
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        System.out.println(linkedList.size());
+//        System.out.println("__________________");
+//
+//        linkedList.replaceElement(7,4);
+//        linkedList.print();
+//        System.out.println("__________________");
+//
+//        System.out.println(linkedList.isEmpty());
+//        System.out.println("__________________");
+//
+//        linkedList.clear();
+//        System.out.println(linkedList.isEmpty());
+//        System.out.println("__________________");
     }
 }
